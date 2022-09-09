42acf92a5a65badf744553741ce510fb84446968
korsakovi <mat2@hotmail.es>
2022-09-09 20:15:00 +0200

    Add CSS as CSS modules

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withTranspileModules = require("next-transpile-modules")([
+ "@korsa/components",
+]);
 
-module.exports = nextConfig
+module.exports = withTranspileModules();
